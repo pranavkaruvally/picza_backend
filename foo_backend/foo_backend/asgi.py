0cc9ce2cabe80e4aa8142d73a6dac5c33faea020
romaljose38 <romaljose38@gmail.com>
2021-06-05 17:38:22 +0530

    asgi.py fix

--- foo_backend/foo_backend/asgi.py
+++ foo_backend/foo_backend/asgi.py
@@ -8,16 +8,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foo_backend.settings")
+asgi_app =  get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foo_backend.settings")
+
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "http": asgi_app,
  
   "websocket": AuthMiddlewareStack(
         URLRouter(
